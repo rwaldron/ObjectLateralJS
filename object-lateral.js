fc3d58892551b526836fc072b0a27b52ad9f2ac1
rwldrn <waldron.rick@gmail.com>
2009-09-12 13:07:11 -0400

    Added: ObjectLateral._unique ObjectLateral._merge
    used by ObjectLateral._extend
    called from ObjectLateral._parser

--- object-lateral.js
+++ object-lateral.js
@@ -6,6 +6,24 @@
     /**
       Private Methods
     */
+    _unique: function(_a) {
+      var _r = [];
+
+      for ( var i = 0, _len = _a.length; i < _len; i++ ) {
+        if ( !$.in_array(_a[i], _r) ) {
+          _r.push(_a[i]);
+        }          
+      }        
+      return _r;
+    },
+    _merge: function( _a, _b ) {
+      var i = 0, _len = _b.length;
+
+      for ( ; i < _len ; i++ ) {
+        _a.push(_b[i]);
+      }
+      return this._unique(_a);
+    },
     _extend:   function(_obj, _new) { 
       for (var _n in _new) { 
         if (_new.hasOwnProperty(_n)) { 
@@ -13,16 +31,18 @@
               _oProp  = _obj[_n]; 
         } 
         _obj[_n] = (_oProp && typeof _nProp == 'object' && typeof _oProp == 'object') ? 
-                      this.merge(_oProp, _nProp) : _nProp; 
+                      this._merge(_oProp, _nProp) : _nProp; 
       } 
       return _obj; 
     },   
     _parser:  function  (_str) {
       //  Even scarier then before.
       var _arr  = eval('[' + _str + ']'), 
+          _len  = _arr.length, 
+          i     = 0, 
           _ret  = {};       
-      
-      for ( var i = 0, _len = _arr.length; i < _len; i++ ) {
+
+      for ( ; i < _len; i++ ) {
         this._extend(_ret, _arr[i]);
       }
       return _ret;
@@ -37,9 +57,8 @@
   window.Lateral = new ObjectLateral();
 })();
 
-
 //  USAGE:
 //  Pointlessly odd format... But who cares, its just for fun right?
-var string       = "{ propA:'dog' },{ propB:'cat', bObj: { a: 'foo', b:'bar', c:'baz' } },{ propC:'hamster', methodA: function() { return 'a method' } }"; 
+var string       = "{ A: { pet:'dog', aArr: ['i', 'am', 'an', 'array'] }, B:{ pet:'cat', bObj: { a: 'foo', b:'bar', c:'baz' } },C:{ pet:'dinosaur', methodA: function() { return 'a method' } }}"; 
 //  http://getfirebug.com or go home.
 console.log( Lateral.toObject(string) );
